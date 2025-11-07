c006376131cec25003e40008010d97150b4a03ad
Nikita <bansalnikita06@gmail.com>
2025-11-08 00:20:31 +0530

    fix routes ordering

--- src/routes/file.ts
+++ src/routes/file.ts
@@ -6,9 +6,9 @@ import express from 'express';
 const router = Router();
 
 router.post('/upload', auth, express.raw({ type: '*/*', limit: '10mb' }), uploadFile);
+router.get("/files/expired-keys", auth, getExpiredFileKeys);
 router.get('/files/:fileId', getFile);
 router.get('/files', auth, getUserFiles);
-router.get("/files/expired-keys", auth, getExpiredFileKeys);
 router.delete("/expired", deleteExpiredFilesApi);
 
 
